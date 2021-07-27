c2c3d995da6af7614fed04556545b85f0ddf51aa
ZhangChi <1030262823@qq.com>
2021-07-27 21:41:29 +0800

    5.0 exercise

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+class Pagelist(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+class Categorylist(admin.ModelAdmin):
+    list_display = ('name','views','likes')
+
+admin.site.register(Category, Categorylist)
+admin.site.register(Page, Pagelist)
+
