d447c93c83e44d1c8e89d0a3b0eefdcf419a60de
ZhangChi <1030262823@qq.com>
2021-07-28 01:02:06 +0800

    6.0 exercise

--- rango/admin.py
+++ rango/admin.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from rango.models import Category, Page
 
 class Pagelist(admin.ModelAdmin):
-    list_display = ('title', 'category', 'url')
+    list_display = ('title', 'category', 'url','views')
 
-class Categorylist(admin.ModelAdmin):
+
+class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name','views','likes')
+    prepopulated_fields = {'slug':('name',)}
 
-admin.site.register(Category, Categorylist)
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, Pagelist)
 
